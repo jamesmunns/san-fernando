fbe33b73feca6abf8b1ef8890685994061266d8f
James Munns <james@onevariable.com>
2022-06-22 10:26:25 +0200

    simplify

--- src/main.rs
+++ src/main.rs
@@ -12,55 +12,48 @@ fn main() {
 fn find_lenstrs(data: &[u8]) {
     let mut data = data;
     let mut leftovers = Vec::new();
-    'outer: loop {
-        loop {
-            if let Some(eidx) = data.iter().position(|&b| b == 0) {
-                // println!("!!!");
-                // hexprint(&data[skip_idx..][..eidx + 1]);
-                // println!();
-                let looky = &data[..=eidx];
-                let mut maybe = None;
-                'search: for (i, b) in looky.iter().rev().enumerate().skip(1) {
-                    if i > 255 {
-                        // println!("FAIL3 {:02X}", b);
-                        break 'search;
-                    } else if *b == (i as u8) {
-                        // println!("MAYBE");
-                        maybe = Some(i);
-                    } else if b.is_ascii_control() {
-                        // println!("FAIL2 {:02X}", b);
-                        break 'search;
-                    } else if !b.is_ascii() {
-                        // println!("FAIL1 {:02X}", b);
-                        break 'search;
-                    }
-                }
 
-                if let Some(found) = maybe.take() {
-                    if found < 3 {
-                        leftovers.extend_from_slice(&data[..=eidx]);
-                    } else {
-                        let len = looky.len();
-                        leftovers.extend_from_slice(&data[..(eidx - found)]);
-                        hexprint(&leftovers);
-                        leftovers.clear();
-                        println!();
-                        print!("Bingo: [{}] ", found);
-                        println!("{}", std::str::from_utf8(&looky[(len - found)..(len - 1)]).unwrap());
-                        println!();
-                    }
-                } else {
-                    leftovers.extend_from_slice(&data[..=eidx]);
-                }
-                data = &data[(eidx + 1)..];
-            } else {
-                hexprint(&leftovers);
-                println!();
-                println!("Done");
-                return;
+    while let Some(eidx) = data.iter().position(|&b| b == 0) {
+        let looky = &data[..=eidx];
+        let mut maybe = None;
+        'search: for (i, b) in looky.iter().rev().enumerate().skip(1) {
+            if i > 255 {
+                // println!("FAIL3 {:02X}", b);
+                break 'search;
+            } else if *b == (i as u8) {
+                // println!("MAYBE");
+                maybe = Some(i);
+            } else if b.is_ascii_control() {
+                // println!("FAIL2 {:02X}", b);
+                break 'search;
+            } else if !b.is_ascii() {
+                // println!("FAIL1 {:02X}", b);
+                break 'search;
             }
         }
+
+        if let Some(found) = maybe.take() {
+            if found < 3 {
+                leftovers.extend_from_slice(looky);
+            } else {
+                let (before, interesting) = looky.split_at(eidx - found + 1);
+                leftovers.extend_from_slice(before);
+                hexprint(&leftovers);
+                leftovers.clear();
+                println!();
+                print!("Bingo: [{}] ", found);
+                println!("{}", std::str::from_utf8(interesting).unwrap());
+                println!();
+            }
+        } else {
+            leftovers.extend_from_slice(looky);
+        }
+        data = &data[(eidx + 1)..];
     }
+
+    hexprint(&leftovers);
+    println!();
+    println!("Done");
 }
 
 fn hexprint(data: &[u8]) {
